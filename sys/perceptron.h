c650cbdde4d80890e5d5fb778a1facff0f5a0225
Quinn Mikelson <mikelson.public@gmail.com>
2016-11-13 15:21:24 -0800

    added perceptron class, copied sample code into N_Network.h
    Sample code and methods pulled from
    https://github.com/BobbyAnguelov/NeuralNetwork/blob/master/Neural%20Network/neuralNetwork.h#L40

---
+++ sys/perceptron.h
@@ -0,0 +1,25 @@
+#ifndef _perceptron
+#define _perceptron
+
+class perceptron{
+//simple prototype "neuron" for network demonstration
+
+//define input variables
+int input;
+
+//define transfer and weighting functions
+void transferMethod();
+
+//define output variable
+bool output;
+
+
+public:
+
+
+};
+
+
+
+
+#endif
