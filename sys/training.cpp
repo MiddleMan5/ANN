f267fa9bb81a5bac74c736c0857ced2e28afd437
Quinn Mikelson <mikelson.public@gmail.com>
2016-11-14 14:00:03 -0800

    added training class definitions

---
+++ sys/training.cpp
@@ -0,0 +1,93 @@
+#ifndef _training
+#define _training
+
+class TrainingData
+{
+public:
+    TrainingData(const std::string filename);
+    bool isEof(void) { return m_trainingDataFile.eof(); }
+    void getTopology(std::vector<unsigned> &topology);
+
+    // Returns the number of input values read from the file:
+    unsigned getNextInputs(t_vals &inputVals);
+    unsigned getTargetOutputs(t_vals &targetOutputVals);
+
+private:
+    std::ifstream m_trainingDataFile;
+};
+
+void TrainingData::getTopology(std::vector<unsigned> &topology)
+{
+    std::string line;
+    std::string label;
+
+    getline(m_trainingDataFile, line);
+    std::stringstream ss(line);
+    ss >> label;
+    if (this->isEof() || label.compare("topology:") != 0) {
+        abort();
+    }
+
+    while (!ss.eof()) {
+        unsigned n;
+        ss >> n;
+        topology.push_back(n);
+    }
+
+    return;
+}
+
+TrainingData::TrainingData(const std::string filename)
+{
+    m_trainingDataFile.open(filename.c_str());
+}
+
+unsigned TrainingData::getNextInputs(t_vals &inputVals)
+{
+    inputVals.clear();
+
+    std::string line;
+    getline(m_trainingDataFile, line);
+    std::stringstream ss(line);
+
+    std::string label;
+    ss>> label;
+    if (label.compare("in:") == 0)
+    {
+        double oneValue;
+
+        while (ss >> oneValue)
+            inputVals.push_back(oneValue);
+    }
+
+    return inputVals.size();
+}
+
+unsigned TrainingData::getTargetOutputs(t_vals &targetOutputVals)
+{
+    targetOutputVals.clear();
+
+    std::string line;
+    getline(m_trainingDataFile, line);
+    std::stringstream ss(line);
+
+    std::string label;
+    ss >> label;
+    if (label.compare("out:") == 0)
+    {
+        double oneValue;
+
+        while (ss >> oneValue)
+            targetOutputVals.push_back(oneValue);
+    }
+
+    return targetOutputVals.size();
+}
+
+
+struct Connection{
+	double weight;
+	double deltaWeight;
+};
+
+#endif
