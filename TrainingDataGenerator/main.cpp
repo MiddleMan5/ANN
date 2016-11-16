65922b08aa66e7889628a50a51605ce5255fd906
Quinn Mikelson <mikelson.public@gmail.com>
2016-11-16 15:03:53 -0800

    Training Program Creates new file now

--- TrainingDataGenerator/main.cpp
+++ TrainingDataGenerator/main.cpp
@@ -1,23 +1,29 @@
 
-
+#include <Windows.h>
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
-
+#include <fstream>
 
 int main()
 {
-	// Random training set for XOR -- two input and one output
+ // CreateFile("func.txt",GENERIC_WRITE,FILE_SHARE_READ, NULL, CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL);
+std::ofstream file;
+file.open("func.txt");
 
-	std::cout << "topology: 2 4 1" << std::endl;
-	for (int i = 5000; i >= 0; --i)
+	file << "topology: 4 6 6" << std::endl;
+	for (int i = 10000; i >= 0; --i)
 	{
-		int n1 = (int)(2.0 * rand() / double(RAND_MAX));
-		int n2 = (int)(2.0 * rand() / double(RAND_MAX));
+		double x = (int)(5 * (rand() / double(RAND_MAX)));
+		double y = (int)(5 * (rand() / double(RAND_MAX)));
 
-		int t = !n1 ^ !n2; // should be 0 xor 1
+		double z = (x*x)+(y*y);
 
-		std::cout << "in: " << n1 << ".0 " << n2 << ".0 " << std::endl;
-		std::cout << "out: " << t << ".0 " << std::endl;
+	//	int t = !n1 ^ !n2; // should be 0 xor 1
+
+		file << "in: " << x << " " << y << std::endl;
+		file << "out: " << z  << std::endl;
 	}
+	file.close();
+	return(1);
 }
---
+++ TrainingDataGenerator/xor.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+
+int main()
+{
+	// Random training set for XOR -- two input and one output
+
+	std::cout << "topology: 2 4 1" << std::endl;
+	for (int i = 5000; i >= 0; --i)
+	{
+		int n1 = (int)(2.0 * rand() / double(RAND_MAX));
+		int n2 = (int)(2.0 * rand() / double(RAND_MAX));
+
+		int t = !n1 ^ !n2; // should be 0 xor 1
+
+		std::cout << "in: " << n1 << ".0 " << n2 << ".0 " << std::endl;
+		std::cout << "out: " << t << ".0 " << std::endl;
+	}
+}
