2428db15165c5cbba89b4ca1df6f1aebd17e7d57
Quinn Mikelson <mikelson.public@gmail.com>
2016-11-23 12:52:49 -0800

    Tiny Adjustment

--- Graphing/errorGraph.py
+++ Graphing/errorGraph.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig = plt.figure()
-ax1 = plt.axes(xlim=(0, 10000), ylim=(0, 300))
-line, = ax1.plot([], [], lw=1)
+ax1 = plt.axes(xlim=(0, 10000), ylim=(0, 50))
+line, = ax1.plot([], [], lw=2)
 
 plt.xlabel('Node Generation Number')
 plt.ylabel('Percent Error')
@@ -24,9 +24,9 @@ def animate(i):
         if len(eachLine)>1:
             x,y = eachLine.split(',')
             xar.append(int(x))
-            yar.append(100*float(y))
+            yar.append(1000*float(y))
     line.set_data(xar, yar)
     return line,
 
-ani = animation.FuncAnimation(fig, animate, init_func=init,frames=20, interval=2, blit=True)
+ani = animation.FuncAnimation(fig, animate, init_func=init,frames=200, interval=20, blit=True)
 plt.show()
